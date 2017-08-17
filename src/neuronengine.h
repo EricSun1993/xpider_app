8d6ac958b038be4188264921e436d524e25f32d7
Ye Tian <flymaxty@foxmail.com>
2017-08-17 15:29:00 +0800

    Fix license problem

--- src/neuronengine.h
+++ src/neuronengine.h
@@ -17,6 +17,7 @@
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **
 ****************************************************************************/
+
 #ifndef NEURONENGINE_H
 #define NEURONENGINE_H
 #include <stdint.h>
